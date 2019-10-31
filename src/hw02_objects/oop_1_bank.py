08b5f237d482a6dee599b6f0a6439c630d6b5f1e
Elias Toama <toama@cip.ifi.lmu.de>
2019-10-31 15:09:33 +0100

    Update oop_1_bank.py

--- src/hw02_objects/oop_1_bank.py
+++ src/hw02_objects/oop_1_bank.py
@@ -29,7 +29,72 @@ class Account:
     which data objects this class is designed for.
     You have to remove the pass statement and then write some
     code for the class. """
-    pass
+    interest_rate = 0.15
+    num_of_accounts = 0
+    # CONSTRUCTOR
+    def __init__(self, num: object, person: object) -> object:
+      self.balance = 0
+      self.number = num
+      self.holder = person
+      self.minBalance = -1000
+      Account.num_of_accounts += 1
+    # METHODS
+    # the method upgrade a balance of an account
+    def deposit(self, amount):
+      self.balance += amount
+
+    # withdrawing money without negative balance
+    def withdraw(self, amount):
+      if amount > self.balance - self.minBalance:
+        amount = self.balance
+      self.balance -= amount
+      return amount
+
+    # the method sets a new holder to an account
+    # validates a new value or raises an exception
+    def set_holder(self, person):
+       if (not type(person) == str):
+           raise TypeError
+       if not (re.match("\w+( \w+)*", person.strip())):
+           raise ValueError
+       self.holder = person
+    # String representation of an object:
+    # the method resturs string that describes an account
+    def __str__(self):
+      res = "*** Account Info ***\n"
+      res += "Account ID:" + str(self.number) + "\n"
+      res += "Holder:" + self.holder + "\n"
+      res += "Balance: " + str(self.balance) + "\n"
+      return res
+
+    # Static method that prints the info about number of excisting accounts
+    @ staticmethod
+    def accounts_info():
+      print(Account.num_of_accounts, "accounts have been created.")
+  
+
+    # applies interesr rate to the balance of the saving account
+    def apply_interest(self):
+     self.balance *= (1 + Account.interest_rate)
+
+
 
 if __name__ == "__main__":
     print("Welcome to the Python Bank!")
+    juliasAccount = Account(1,"Julia")
+    eliassAccount = Account(2, "Elias")
+
+    juliasAccount.deposit(200)
+    juliasAccount.withdraw(100)
+    print(juliasAccount)
+    juliasAccount.set_holder("David")
+    juliasAccount.apply_interest()
+    print(juliasAccount)
+
+
+
+    eliassAccount.deposit(500)
+    eliassAccount.withdraw(2000)
+    print(eliassAccount)
+
+    Account.accounts_info()
