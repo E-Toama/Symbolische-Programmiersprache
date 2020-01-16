e86d7e17485f68e6fcb74b105a5d3f77cd681997
Elias Toama <toama@cip.ifi.lmu.de>
2020-01-16 14:44:29 +0100

    Update model_lang.py

--- src/hw08_nltk/model_lang.py
+++ src/hw08_nltk/model_lang.py
@@ -1,4 +1,6 @@
 import nltk
+import collections
+from nltk.corpus import udhr
 
 
 class LangModeler(object):
@@ -7,14 +9,22 @@ class LangModeler(object):
         self.words = words
 
     def build_language_models(self):
-        # TODO return ConditionalFrequencyDistribution of words in the UDHR corpus conditioned on each language
         # hint: use nltk.ConditionalFreqDist
-        pass
+        return nltk.ConditionalFreqDist((language, word.lower())
+                                        for language in self.languages
+                                        for word in udhr.words(language + '-Latin1'))
 
-    def guess_language(self,language_model_cfd, text):
+    def guess_language(self, language_model_cfd, text):
         """Returns the guessed language for the given text"""
 
-        #TODO for each language calculate the overall score of a given text
-        #based on the frequency of words accessible by language_model_cfd[language].freq(word) and then
-        #identify most likely language for a given text according to this score
-        pass
+        # based on the frequency of words accessible by language_model_cfd[language].freq(word) and then
+        # identify most likely language for a given text according to this score
+        results = {lang: 0 for lang in self.languages}
+        token = nltk.word_tokenize(text)
+        for word in token:
+            for language in self.languages:
+                results[language] += language_model_cfd[language][word]
+
+        results = [lang for lang, _ in sorted(results.items(), key=lambda s: s[1], reverse=True)]
+
+        return results[0]
